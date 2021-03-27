86c3341525a1914c68620241f57592484c14624a
Shubham96srj <jaiswals821@gmail.com>
2021-03-27 03:08:33 +0000

    real mearge

---
+++ merge.java
@@ -0,0 +1,15 @@
+Filesystem      Size  Used Avail Use% Mounted on
+/dev/root       7.7G  1.6G  6.2G  21% /
+devtmpfs        484M     0  484M   0% /dev
+tmpfs           490M     0  490M   0% /dev/shm
+tmpfs            98M  788K   98M   1% /run
+tmpfs           5.0M     0  5.0M   0% /run/lock
+tmpfs           490M     0  490M   0% /sys/fs/cgroup
+/dev/loop0       34M   34M     0 100% /snap/amazon-ssm-agent/3552
+/dev/loop1       56M   56M     0 100% /snap/core18/1988
+/dev/loop2       70M   70M     0 100% /snap/lxd/19188
+/dev/loop3       71M   71M     0 100% /snap/lxd/19647
+/dev/loop4       33M   33M     0 100% /snap/snapd/11107
+/dev/loop5       33M   33M     0 100% /snap/snapd/11402
+tmpfs            98M     0   98M   0% /run/user/1000
+insert 
